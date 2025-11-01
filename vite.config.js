69c1b1c979ce98d52cf44e1b2a07ea287d1eddea
Marlon <marlonrodriguezalvarez890@gmail.com>
2025-11-01 18:33:30 -0400

    cambios

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base : '/Proyecto-Vue-PW/',
   plugins: [
     vue(),
     vueDevTools(),
